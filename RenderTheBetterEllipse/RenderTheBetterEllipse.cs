c44cb0c7c0c5f08a2d114b481e122c9aea2a20be
Kwonbareum <Kwonbareum@gmail.com>
2024-11-19 08:52:39 +0900

    RenderTheBetterEllipse

---
+++ RenderTheBetterEllipse/RenderTheBetterEllipse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace RenderTheBetterEllipse
+{
+    class RenderTheBetterEllipse : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new RenderTheBetterEllipse());
+        }
+
+        public RenderTheBetterEllipse()
+        {
+            Title = "Render the Better Ellipse";
+
+            BetterEllipse elips = new BetterEllipse();
+            elips.Fill = Brushes.AliceBlue;
+            elips.Stroke = new Pen(new LinearGradientBrush(Colors.CadetBlue, Colors.Chocolate, new Point(1, 0), new Point(0, 1)), 24);
+
+            Content = elips;
+        }
+    }
+}
