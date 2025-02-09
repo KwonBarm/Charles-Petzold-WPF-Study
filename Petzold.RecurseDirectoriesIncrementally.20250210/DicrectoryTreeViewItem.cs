0940e58f0cfb31a5887a590808fd82a4d23d1ec5
Kwonbareum <Kwonbareum@gmail.com>
2025-02-10 08:59:00 +0900

    20250210

---
+++ Petzold.RecurseDirectoriesIncrementally.20250210/DicrectoryTreeViewItem.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Petzold.RecurseDirectoriesIncrementally._20250210
+{
+    class DicrectoryTreeViewItem : ImagedTreeViewItem
+    {
+        DirectoryInfo dir;
+
+        // DirectoriyInfo 객체를 받는 생성자
+        public DicrectoryTreeViewItem(DirectoryInfo dir)
+        {
+            this.dir = dir;
+            Text = dir.Name;
+
+            SelectedImage = new BitmapImage(new Uri("pack://application,,/Images/OPENFOLD.png"));
+            UnSelectedImage = new BitmapImage(new Uri("pack://application,,/Images/CLOSEFOLD.png"));
+        }
+
+        // DirectoryInfo 객체에 대한 Public 프로퍼티
+        public DirectoryInfo DirectoryInfo
+        {
+            get { return dir; }
+        }
+
+        // 항목들과 값을 연동시켜주는 Public 메서드 
+        public void Populate()
+        {
+            Items.Clear();
+            DirectoryInfo[] dirs;
+
+            try
+            {
+                dirs = dir.GetDirectories();
+            }
+            catch
+            {
+                return;
+            }
+
+            foreach (DirectoryInfo dirChild in dirs)
+                Items.Add(new DicrectoryTreeViewItem(dirChild));
+
+            // populate 하위 항목에 대한 이벤트 오버라이딩
+        }
+
+        protected override void OnExpanded(RoutedEventArgs e)
+        {
+            base.OnExpanded(e);
+
+            foreach (object obj in Items)
+            {
+                DicrectoryTreeViewItem item = obj as DicrectoryTreeViewItem;
+                item.Populate();
+            }
+        }
+    }
+}
---
+++ Petzold.RecurseDirectoriesIncrementally.20250210/ImagedTreeViewItem.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Petzold.RecurseDirectoriesIncrementally._20250210
+{
+    class ImagedTreeViewItem : TreeViewItem
+    {
+        TextBlock text;
+        Image img;
+        ImageSource srcSelected, srcUnselected;
+
+        // 생성자는 이미지와 텍스트를 스택패널에 추가
+        public ImagedTreeViewItem()
+        {
+            StackPanel stack = new StackPanel();
+            stack.Orientation = Orientation.Horizontal;
+            Header = stack;
+
+            img = new Image();
+            img.VerticalAlignment = VerticalAlignment.Center;
+            img.Margin = new Thickness(0, 0, 2, 0);
+            stack.Children.Add(img);
+
+            text = new TextBlock();
+            text.VerticalAlignment = VerticalAlignment.Center;
+            stack.Children.Add(text);
+        }
+
+        // 텍스트와 이미지에 대한 Public 속성
+        public string Text
+        {
+            set { text.Text = value; }
+            get { return text.Text; }
+        }
+
+        public ImageSource SelectedImage
+        {
+            set
+            {
+                srcSelected = value;
+
+                if (IsSelected)
+                    img.Source = srcSelected;
+            }
+            get { return srcSelected; }
+        }
+
+        public ImageSource UnSelectedImage
+        {
+            set
+            {
+                srcUnselected = value;
+
+                if (!IsSelected)
+                    img.Source = srcUnselected;
+            }
+            get { return srcUnselected; }
+        }
+
+        // 이미지를 설정하는 이벤트 오버라이드
+        protected override void OnSelected(RoutedEventArgs e)
+        {
+            base.OnSelected(e);
+            img.Source = srcSelected;
+        }
+        protected override void OnUnselected(RoutedEventArgs e)
+        {
+            base.OnUnselected(e);
+            img.Source = srcUnselected;
+        }
+
+    }
+}
