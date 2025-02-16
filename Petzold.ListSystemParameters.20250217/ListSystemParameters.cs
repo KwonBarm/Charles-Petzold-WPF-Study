18745637b72e7ad3bfc5315f9eb6cf28dfa20622
Kwonbareum <Kwonbareum@gmail.com>
2025-02-17 08:06:54 +0900

    20250217

---
+++ Petzold.ListSystemParameters.20250217/ListSystemParameters.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Petzold.ListSystemParameters._20250217
+{
+    internal class ListSystemParameters : Window
+    {
+        [STAThread]
+        public static void Main() 
+        {
+            Application app = new Application();
+            app.Run(new ListSystemParameters());
+        }
+
+        public ListSystemParameters()
+        {
+            Title = "List System Parameters";
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            // 윈도우 Content를 위한 ListView 생성
+            ListView lstvue = new ListView();
+            Content = lstvue;
+
+            // ListView의 View로 사용하기 위해 GridView 생성
+            GridView grdvue = new GridView();
+            lstvue.View = grdvue;
+
+            // GridView 열 2개 생성
+            GridViewColumn col = new GridViewColumn();
+            col.Header = "Property Name";
+            col.Width = 200;
+            col.DisplayMemberBinding = new Binding("Name");
+            grdvue.Columns.Add(col);
+
+            col = new GridViewColumn();
+            col.Header = "Value";
+            col.Width = 200;
+            col.DisplayMemberBinding = new Binding("Value");
+            grdvue.Columns.Add(col);
+
+            // 배열에서 모든 시스템 파라미터를 구함
+            PropertyInfo[] props = typeof(SystemParameters).GetProperties();
+
+            // ListView에 항목 추가
+            foreach (PropertyInfo prop in props)
+            {
+                if (prop.PropertyType != typeof(ResourceKey))
+                {
+                    SystemParam sysparma= new SystemParam();
+                    sysparma.Name = prop.Name;
+                    sysparma.Value = prop.GetValue(null, null);
+                    lstvue.Items.Add(sysparma);
+                }
+            }
+        }
+    }
+}
---
+++ Petzold.ListSystemParameters.20250217/SystemParam.cs
@@ -0,0 +1,25 @@
+﻿namespace Petzold.ListSystemParameters._20250217
+{
+    internal class SystemParam
+    {
+        string strName;
+        object objValue;
+
+        public string Name
+        {
+            get { return strName; }
+            set { strName = value; }
+        }
+
+        public object Value
+        {
+            get { return objValue; }
+            set { objValue = value; }
+        }
+
+        public override string ToString()
+        {
+            return Name + " = " + Value;
+        }
+    }
+}
