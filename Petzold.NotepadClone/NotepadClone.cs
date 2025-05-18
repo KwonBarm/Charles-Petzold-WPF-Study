bccdcb1cce9906229a3d6fbdb97a24a0cbfc1eb1
Kwonbareum <Kwonbareum@gmail.com>
2025-05-19 08:04:44 +0900

    20250519

---
+++ Petzold.NotepadClone/NotepadClone.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Petzold.NotepadClone
+{
+    public partial class NotepadClone : Window
+    {
+        protected string strAppTitle; // 프로그램의 타이틀바 이름
+        protected string strAppData;  // 설정 파일의 전체 파일 이름
+        protected NotepadCloneSettings settings; // 설정
+        protected bool isFillDirty = false; // 파일 저장 확인을 위한 플래그
+
+        // 메인 윈도우에서 사용되는 컨트롤
+        protected Menu menu;
+        protected TextBox txtbox;
+        protected StatusBar status;
+
+        string strLoadedFile; // 불러올 파일의 전체 이름
+        StatusBarItem statLineCol; // 줄과 열을 표시하는 상태바 아이템
+
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.ShutdownMode = ShutdownMode.OnMainWindowClose; // 메인 윈도우가 닫힐 때 애플리케이션 종료
+            app.Run(new NotepadClone());
+        }
+
+        public NotepadClone()
+        {
+            // 속성에 접근하기 위해 현재 실행중인 어셈블리의 정보를 가져옴
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            // assembly.GetCustomAttributes(...) Assembly에 붙은 커스텀 특성들 중 AssemblyTitleAttribute를 가져옴
+            // false는 상속된 특성은 제외하라는 의미
+            // 이 속성은 일반적으로 AssemblyInfo.cs 파일에 정의되어 있음
+            // [assembly : AssemblyTitle("NotepadClone")]
+            AssemblyTitleAttribute title = (AssemblyTitleAttribute)assembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0];
+            strAppTitle = title.Title;
+        }
+
+    }
+}
---
+++ Petzold.NotepadClone/NotepadCloneSettings.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace Petzold.NotepadClone
+{
+    public class NotepadCloneSettings
+    {
+        // 기본 설정
+        public WindowState WindowState = WindowState.Normal;
+        public Rect RestoreBound = Rect.Empty;
+        public TextWrapping TextWrapping = TextWrapping.NoWrap;
+        public string FontFamily = "";
+        public string FontStyle = new FontStyleConverter().ConvertToString(FontStyles.Normal);
+        public string FontWeight = new FontWeightConverter().ConvertToString(FontWeights.Normal);
+        public string FontStretch = new FontStretchConverter().ConvertToString(FontStretches.Normal);
+        public double FontSize = 11;
+
+        // 현재 객체를 XML로 직렬화하여 지정된 파일에 저장
+        public virtual bool Save(string strAppData)
+        {
+            try
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(strAppData)); // strAppData = "C:\\Temp\\MyData\\NotepadCloneSettings.xml" → "C:\\Temp\\MyData" 폴더가 없으면 생성
+                StreamWriter write = new StreamWriter(strAppData); // strAppData 경로로 Stream을 쓰기 모드로 염, 파일이 있으면 덮어씀
+                XmlSerializer xml = new XmlSerializer(GetType()); // GetType()은 현재 객체의 타입을 반환 
+                xml.Serialize(write, this); // this 객체를 XML형식으로 직렬화하여 write에 저장
+                write.Close();
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // 설정을 파일에서 불러들임
+        public static object Load(Type type, string strAppData)
+        {
+            StreamReader reader;
+            object settings;
+            XmlSerializer xml = new XmlSerializer(type);
+
+            try
+            {
+                reader = new StreamReader(strAppData); // strAppData 경로로 Stream을 읽기 모드로 염, 파일이 없으면 예외 발생
+                settings = xml.Deserialize(reader); // reader에서 XML형식으로 직렬화된 객체를 역직렬화하여 settings에 저장
+                reader.Close();
+            }
+            catch
+            {
+                settings = type.GetConstructor(System.Type.EmptyTypes).Invoke(null); // 역직렬화에 실패하면 기본 생성자를 호출하여 새로운 객체를 생성
+            }
+
+            return settings; // 역직렬화된 객체를 반환
+        }
+    }
+}
