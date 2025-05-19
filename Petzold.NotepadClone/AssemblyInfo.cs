739bcc78a4b2a83920d5bbf3fb26f908d5349598
Kwonbareum <Kwonbareum@gmail.com>
2025-05-20 08:47:03 +0900

    20250520

--- Petzold.NotepadClone/AssemblyInfo.cs
+++ Petzold.NotepadClone/AssemblyInfo.cs
@@ -1,10 +1,22 @@
+using System.Reflection;
 using System.Windows;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
                                                 //(used if a resource is not found in the page,
                                                 // or application resource dictionaries)
+
+    // /Themes/Generic.xaml에 있는 ResourceDictionary를 자동으로 찾아서 사용
     ResourceDictionaryLocation.SourceAssembly   //where the generic resource dictionary is located
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
+
+// Visual Studio에서 자동 생성된 AssemblyInfo.cs 파일은 주석 처리
+// [assembly: AssemblyTitle("NotepadClone")] 
+// [assembly: AssemblyProduct("NotepadClone")]
+// [assembly: AssemblyCompany("www.charlespetzold.com")]
+// [assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("A simple notepad clone application.")]
+[assembly: AssemblyCopyright("\x00A9 2006 by Charles Petzold")]
--- Petzold.NotepadClone/NotepadClone.cs
+++ Petzold.NotepadClone/NotepadClone.cs
@@ -1,7 +1,10 @@
-﻿using System.Reflection;
+﻿using System.IO;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Petzold.NotepadClone
 {
@@ -30,6 +33,8 @@ namespace Petzold.NotepadClone
 
         public NotepadClone()
         {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
             // 속성에 접근하기 위해 현재 실행중인 어셈블리의 정보를 가져옴
             Assembly assembly = Assembly.GetExecutingAssembly();
 
@@ -37,9 +42,98 @@ namespace Petzold.NotepadClone
             // false는 상속된 특성은 제외하라는 의미
             // 이 속성은 일반적으로 AssemblyInfo.cs 파일에 정의되어 있음
             // [assembly : AssemblyTitle("NotepadClone")]
+            // AssemblyTitleAttribute는 하나의 속성만을 가지므로 [0]으로 첫 번째 요소를 가져옴
             AssemblyTitleAttribute title = (AssemblyTitleAttribute)assembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0];
             strAppTitle = title.Title;
+
+            // strAppData 파일 이름을 설정하기 위해 AssemblyProduct 속성을 구함
+            // Environment.SpecialFolder.ApplicationData는 C:\Users\사용자\AppData\Roaming 폴더를 가리킴
+            AssemblyProductAttribute product = (AssemblyProductAttribute)assembly.GetCustomAttributes(typeof(AssemblyProductAttribute), false)[0];
+            strAppData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"Petzold\\" + product.Product + "\\" + product.Product+".Settings.xml");
+
+            // Window Content를 위해 DockPanel을 생성
+            DockPanel dock = new DockPanel();
+            Content = dock;
+
+            // 상단에 위치할 Menu를 생성
+            menu = new Menu();
+            dock.Children.Add(menu);
+            DockPanel.SetDock(menu, Dock.Top);
+
+            // 하단에 위치할 상태바 설정
+            status = new StatusBar();
+            dock.Children.Add(status);
+            DockPanel.SetDock(status, Dock.Bottom);
+
+            // 선과 열을 보여주기 위해 StatusBarItem을 생성
+            statLineCol = new StatusBarItem();
+            statLineCol.HorizontalAlignment = HorizontalAlignment.Right;
+            status.Items.Add(statLineCol);
+            DockPanel.SetDock(statLineCol, Dock.Right);
+
+            // 클라이언트 영역의 남은 부분을 채울 TextBox를 생성
+            txtbox = new TextBox();
+            txtbox.AcceptsReturn = true; // Enter키를 눌렀을 때 줄바꿈이 가능하도록 설정
+            txtbox.AcceptsTab = true; // Tab키를 눌렀을 때 탭이 가능하도록 설정
+            txtbox.VerticalScrollBarVisibility = ScrollBarVisibility.Auto; // 세로 스크롤바 자동 표시
+            txtbox.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto; // 가로 스크롤바 자동 표시
+            txtbox.TextChanged += TextBoxOnTextChanged;
+            txtbox.SelectionChanged += TextBoxOnSelectionChanged;
+            dock.Children.Add(txtbox);
+
+            // 모든 탑 레벨 메뉴 항목 생성
+            // AddFileMenu(menu);
+            // AddEditMenu(menu);
+            // AddFormatMenu(menu);
+            // AddViewMenu(menu);
+            // AddHelpMenu(menu);
+
+            // 이전에 실행되면서 저장해놓은 설정을 불러옴
+            settings = (NotepadCloneSettings)LoadSettings();
+
+            // 저장된 설정을 적용
+            WindowState = settings.WindowState;
+
+            if(settings.RestoreBound != Rect.Empty)
+            {
+                Left = settings.RestoreBound.Left;
+                Top = settings.RestoreBound.Top;
+                Width = settings.RestoreBound.Width;
+                Height = settings.RestoreBound.Height;
+            }
+
+            txtbox.TextWrapping = settings.TextWrapping;
+            txtbox.FontFamily = new FontFamily(settings.FontFamily);
+            txtbox.FontStyle = (FontStyle)new FontStyleConverter().ConvertFromString(settings.FontStyle);
+            txtbox.FontWeight = (FontWeight)new FontWeightConverter().ConvertFromString(settings.FontWeight);
+            txtbox.FontStretch = (FontStretch)new FontStretchConverter().ConvertFromString(settings.FontStretch);
+            txtbox.FontSize = settings.FontSize;
+
+            // Loaded 이벤트 핸들러를 설정
+            Loaded += WindowOnLoaded;
+
+            // TextBox에 Focus 설정
+            txtbox.Focus();
         }
 
+        // 생성자를 호출했을 때 설정을 불러들이는 메서드를 오버라이딩
+        protected virtual object LoadSettings()
+        {
+            return NotepadCloneSettings.Load(typeof(NotepadCloneSettings),strAppData);
+        }
+
+
+        private void WindowOnLoaded(object sender, RoutedEventArgs e)
+        {
+            //ApplicationCommands.New(WPF가 제공하는 표준 명령 RoutedCommand)에 연결된 이벤트 핸들러를 실행
+            ApplicationCommands.New.Execute(null, this); // New 명령을 실행하여 새 문서 생성
+        }
+
+        private void TextBoxOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+        private void TextBoxOnSelectionChanged(object sender, RoutedEventArgs e)
+        {
+        }
     }
 }
